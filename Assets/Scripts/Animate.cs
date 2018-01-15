729254ee5537740b4b83640c4047d8969dbb9632
Milas Baumgarten <milas.baumgarten@freenet.de>
2018-01-16 00:51:57 +0100

    implemented Edge animation (basic)
    Animationsqualität wird noch ausgebaut, Features fehlen (Breite, Farbe)

--- Assets/Scripts/Animate.cs
+++ Assets/Scripts/Animate.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Animate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public GameObject edge;
+
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Space)){
+			if (edge != null){
+				AnimateEdge edgeAnim = edge.GetComponent<AnimateEdge>();
+				if (edgeAnim != null){
+					if (edgeAnim.getIsFinished()){
+						edgeAnim.setDestination(Random.Range(-5,5), Random.Range(-5,5));
+						//edgeAnim.setDestination(-2, 0);
+						edgeAnim.startAnimation();
+					}
+				}
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/AnimateEdge.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateEdge : MonoBehaviour {
+
+	// Dauer der Animation
+	public float animationDuration = 1f;
+	private float currentTime = 0f;
+	public float speed = 3f;
+
+	// Zielposition bzw. Ende der Kante ab
+	[SerializeField]
+	Vector2 destinationPos = new Vector2();
+
+	// ob Kante gerade animiert wird
+	bool animating = false;
+
+	// verkürze Kommunikation mit LineRenderer Komponente über Variable
+	LineRenderer line;
+
+	void Start(){
+		line = GetComponent<LineRenderer>();
+	}
+
+	void LateUpdate () {
+		if (animating){
+			animate();
+		}
+	}
+
+	void animate(){
+		// speichere Startpunkt um Zielpunktberechnung übersichtlicher zu machen
+			Vector2 start = line.GetPosition(0);
+			// setze und berechne Zielpunkt
+			//	- errechne Richtungsvektor (normalisiert)
+			//	- multipliziere Richtungsvektor mit Geschwindigkeit und vergangener Zeit
+			line.SetPosition(1, start + (destinationPos - start).normalized * speed * currentTime);
+
+			// erhöhe "Zeitmessung"
+			currentTime += Time.deltaTime;
+
+			// Animation ist beendet, sobald wirkliche Länge der Kante > gewollte Länge
+			if (Vector2.Distance(start, destinationPos) < Vector2.Distance(start, line.GetPosition(1))){
+				line.SetPosition(1, destinationPos);
+				animating = false;
+				currentTime = 0;
+			}
+	}
+
+	// rufe auf um Animation der Kante zu starten
+	public void startAnimation(){
+		animating = true;
+	}
+
+//#####################################################################
+// GET			SET
+
+	// übergebe Zielposition bzw. Ende der Kante
+	// Startposition = transform.position (schon bei der Initialisierung gesetzt)
+	public void setDestination(float x, float y){
+		// Teste ob Start == Ziel
+		// sollte NIE gewollt sein, sonst ist keine Animation möglich
+		if (line.GetPosition(0) != new Vector3(x,y,0)){
+			destinationPos = new Vector2(x,y);
+
+			line.SetPosition(1, destinationPos);
+		}
+		else{
+			Debug.LogError( "Zielposition darf NICHT gleich Startposition sein!! Wurde auf (0,0,0) gesetzt.\n" +
+							"- LG Milas");
+		}
+		
+	}
+
+	// gibt zurück ob Animation abgeschlossen ist
+	// -> gibt Negation von animating zurück
+	public bool getIsFinished(){
+		return !animating;
+	}
+}
+//23:30
