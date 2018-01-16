f6342ea4bb891b9156dde4288e07da09a9b8ed97
Unknown <marisa-sophia@gmx.de>
2018-01-16 15:26:10 +0100

    Tabellen

---
+++ Assets/Scripts/AddNewRow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddNewRow : MonoBehaviour {
+
+    public Transform contentPanel;
+    public SimpleObjectPool rowObjectPool;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddRow()
+    {
+        rowObjectPool.GetObject(contentPanel);
+    }
+}
