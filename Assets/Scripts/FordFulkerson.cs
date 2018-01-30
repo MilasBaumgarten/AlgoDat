5ec45ce646e5c02856fa58fc96d5efeec40694c7
Milas Baumgarten <milas.baumgarten@freenet.de>
2018-01-30 15:04:59 +0100

    Fix FFA

--- Assets/Scripts/FordFulkerson.cs
+++ Assets/Scripts/FordFulkerson.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using System;
 using AnimationQueue;
 using Model;
+using TMPro;
 
 public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 
 	private static bool run; //gibt Auskunft darüber, ob Algorithmus ausgeführt werden kann
-	private static int leftCapacity; //gibt Auskunft über die auf der Edge verbleibende Kapazität
 	private List<int> minimalFlow = new List<int>(); //gibt Auskunft über die im aktuellen Druchlauf kleinste benutzte Kapazität, die danach addiert/subtrahiert wird
 	private static List<Edge> Wayforward = new List<Edge>(); //gibt Auskunft über die beim aktuellen Durchgang abgelaufenen Edges in Vorwärtige Richtung
 	private static List<Edge> Waybackward = new List<Edge>(); //gibt Auskunft über die beim aktuellen Durchgang abgelaufenen Edges in Rückwärtige Richtung
@@ -17,11 +17,11 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 	public static Node actualN;
 	CController Cc;
 	private List<bool> wentForward = new List<bool>(); // speichert in welche Richtung sich bewegt wurde
-	private int killswitch = 50;
+
+	public TextMeshProUGUI debugText; // Ausgabe in GUI
 
 	void Start(){
-		GameObject sd = GameObject.FindWithTag("GraphController");
-		Cc = sd.GetComponent<CController>();
+		Cc = GameObject.FindWithTag("GraphController").GetComponent<CController>();
 	}
 
 	private void vorarbeit(){ // Methode, welche grundlegende Schritte ausführt, um FFA (Ford Fulkerson Algorithmus) funktionsfähig zu machen
@@ -31,7 +31,6 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 			//Variablen der Klasse zurücksetzen, um eventuell zwischengespeicherte Werte bei Neustart zu löschen
 			run = true;
 			minimalFlow.Clear();
-			leftCapacity = 0;
 
 			Waybackward.Clear();
 			Wayforward.Clear();
@@ -50,11 +49,6 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 	}
 
 	public void fordFulkerson(bool walkThrough){ // eigentlicher Algorithmus, Walkthrough Variable gibt Auskunft darüber, welche Darstellungsart Nutzer angecklickt hat -> Schrittweise oder am Stück
-		killswitch--;
-		if (killswitch < 0){
-			kill();
-			return;
-		}
 		
 		vorarbeit (); // oben vorbereitet und erläuterte Vorarbeit einmalig ausführen vor Start 
 		if (run) { // Test, ob Algorithmus noch ausgeführt werden soll bzw. darf
@@ -71,6 +65,7 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 					if(actualN.getName().Equals(actualEdge.getStart())){
 						Wayforward.Add (actualEdge);// ausgewählte Kante wird dem Wegearray hinzugefügt und sich gemerkt, um später den Weg von Queelle zu Senke rekonstruieren zu können
 						actualEdge.setVisited(true);
+						wentForward.Add(true);
 					}
 
 					actualN = Cc.GetNodeAsObject(actualEdge.getEnd ());// zu betrachtender Knoten wird gewechselt auf den Knoten, der am Ende der Edge befindlich ist (Frage bei Rückläufigen Edges???)
@@ -78,16 +73,10 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 					flowThroughEdge(actualN);
 				}
 			}
-			//kill();
 		}
 	}
 
 	private void flowThroughEdge(Node currentNode){
-		killswitch--;
-		if (killswitch < 0){
-			kill();
-			return;
-		}
 		// mit Quellknoten verbundene Kanten werden ermittelt und zwischengespeichert
 		List<Edge> connectedEdges = new List<Edge>(currentNode.getConnectedEdges());
 		Edge currentEdge;
@@ -121,10 +110,6 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 				Waybackward.Add (currentEdge);// ausgewählte Kante wird dem Wegearray hinzugefügt und sich gemerkt, um später den Weg von Queelle zu Senke rekonstruieren zu können
 				wentForward.Add(false);
 
-				string tmp = currentEdge.getStart();
-				currentEdge.setStart(currentEdge.getEnd());
-				currentEdge.setEnd(tmp);
-
 				minimalFlow.Add(currentEdge.getCapacity());
 
 				// durchlaufe nächste Kante
@@ -151,73 +136,71 @@ public class FordFulkerson : MonoBehaviour { //Erstellt von Tim und Sebastian
 		minimalFlow.Sort();
 		
 		MaxFlow += minimalFlow[0]; // Inkrimentierung des Maximalen Flusses, um den, im aktuellen Durchgang, kleinsten benutzten Fluss
-		// TODO: fixe Animationsreihenfolge ()
-		for (int i = 0; i < Wayforward.Count; i++) {//Iteration über alle im Weg zwischen Quelle und Senke befindlichen Kanten des aktuellen Durchlaufes
-				Wayforward [i].setFlow(Wayforward [i].getFlow() + minimalFlow[0]);
-				AnimationManager.AM.addAnimation(new FillEdgeForward(Wayforward [i].getObject(), Wayforward [i].getFlow(), Wayforward [i].getCapacity()));
-		}
-		for (int i = 0; i < Waybackward.Count; i++){
-				Waybackward [i].setFlow (Waybackward [i].getFlow () - minimalFlow[0]);// Erhöhung verbleibender Kapazität der Kanten, um deren aktuellen rückläufigen Durchfluss
+
+		int indexForward = 0;
+		int indexBackward = 0;
+		foreach (bool wf in wentForward){
+			if (wf){
+				Wayforward [indexForward].setFlow(Wayforward [indexForward].getFlow() + minimalFlow[0]);
+				AnimationManager.AM.addAnimation(new FillEdgeForward(Wayforward [indexForward].getObject(), Wayforward [indexForward].getFlow(), Wayforward [indexForward].getCapacity()));
+				indexForward++;
+			}
+			else{
+				Waybackward [indexBackward].setFlow (Waybackward [indexBackward].getFlow () - minimalFlow[0]);// Erhöhung verbleibender Kapazität der Kanten, um deren aktuellen rückläufigen Durchfluss
 				// WIP Animation
-				//Vector3 lineStart = Waybackward [i].getObject().GetComponent<LineRenderer>().GetPosition(0);
-				//Vector3 lineEnd = Waybackward [i].getObject().GetComponent<LineRenderer>().GetPosition(1);
-				//Waybackward [i].getObject().GetComponent<LineRenderer>().SetPositions(new Vector3[] {lineEnd, lineStart});
-				AnimationManager.AM.addAnimation(new FillEdgeForward(Waybackward [i].getObject(), Waybackward [i].getFlow(), Waybackward [i].getCapacity()));
+				//Vector3 lineStart = Waybackward [indexBackward].getObject().GetComponent<LineRenderer>().GetPosition(0);
+				//Vector3 lineEnd = Waybackward [indexBackward].getObject().GetComponent<LineRenderer>().GetPosition(1);
+				//Waybackward [indexBackward].getObject().GetComponent<LineRenderer>().SetPositions(new Vector3[] {lineEnd, lineStart});
+				AnimationManager.AM.addAnimation(new FillEdgeForward(Waybackward [indexBackward].getObject(), Waybackward [indexBackward].getFlow(), Waybackward [indexBackward].getCapacity()));
+				indexBackward ++;
+			}
 		}
+
+		wentForward.Clear();
 
 		if(walkThrough){ //Test ob kompletter Durchlauf angewählt wurde
 			fordFulkerson (true);//rekursiver Neuaufruf des FFA
 		}
 	}
 
-	private void kill(){ //Beendungsmethode
-			//Ausgabe auf Ausgabelabelfeld anpassen!!!!!!
-			Debug.Log("MaxFlow: " + MaxFlow);	//Ausgabe des maximalen Flusses
-			run = false;//Ausführvariabel auf Stopp setzen
-	}
-
 	// gehe zu letztem besuchten Knoten zurück, mache Änderungen auf dem Weg rückgängig
 	private void goBack(){
-		if (minimalFlow.Count > 1){
+		if (minimalFlow.Count >= 1){
 			minimalFlow.RemoveAt(minimalFlow.Count - 1);
 		}
 
 		// lösche letzten besuchten Knoten aus Wegliste
-		if(wentForward[wentForward.Count - 1]){
-			Wayforward.RemoveAt(Wayforward.Count - 1);
-		}
-		else{
-			// mache Richtungsänderung rückgängig
-			string tmp = Waybackward[Waybackward.Count - 1].getStart();
-			Waybackward[Waybackward.Count - 1].setStart(Waybackward[Waybackward.Count - 1].getEnd());
-			Waybackward[Waybackward.Count - 1].setEnd(tmp);
-
-			Waybackward.RemoveAt(Waybackward.Count - 1);
-		}
-
-		wentForward.RemoveAt(wentForward.Count - 1);
-
-		// gehe zu vorletzetem besuchten Knoten zurück
-		if (wentForward[wentForward.Count - 1]){
-			wentForward.RemoveAt(wentForward.Count - 1);
-
-			if (Wayforward.Count >= 1){
-				flowThroughEdge(Cc.GetNodeAsObject(Wayforward[Wayforward.Count - 1].getEnd()));
+		if (wentForward.Count > 1){
+			if(wentForward[wentForward.Count - 1]){
+				Wayforward.RemoveAt(Wayforward.Count - 1);
 			}
 			else{
-				terminate(false);
-				kill();
+				Waybackward.RemoveAt(Waybackward.Count - 1);
+			}
+
+			wentForward.RemoveAt(wentForward.Count - 1);
+
+			// gehe zu vorletzetem besuchten Knoten zurück
+			if (wentForward[wentForward.Count - 1]){
+				if (Wayforward.Count >= 1){
+					flowThroughEdge(Cc.GetNodeAsObject(Wayforward[Wayforward.Count - 1].getEnd()));
+				}
+			}
+			else{
+				if (Waybackward.Count >= 1){
+					flowThroughEdge(Cc.GetNodeAsObject(Waybackward[Waybackward.Count - 1].getStart()));
+				}
 			}
 		}
 		else{
-			wentForward.RemoveAt(wentForward.Count - 1);
-			if (Waybackward.Count >= 1){
-				flowThroughEdge(Cc.GetNodeAsObject(Waybackward[Waybackward.Count - 1].getStart()));
-			}
-			else{
-				terminate(false);
-				kill();
-			}
+			kill();
 		}
+	}
+
+	private void kill(){ //Beendungsmethode
+			//Ausgabe auf Ausgabelabelfeld anpassen!!!!!!
+			Debug.Log("MaxFlow: " + MaxFlow);	//Ausgabe des maximalen Flusses
+			debugText.text = "MaxFlow: " + MaxFlow;
+			run = false;//Ausführvariabel auf Stopp setzen
 	}
 }
